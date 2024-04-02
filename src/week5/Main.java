198ae33ec0273018f487d84207c0195bf0fd3632
01 <talhwan@hotmail.com>
2024-04-02 15:35:41 +0900

    j002
    main, readme, screenshot

--- src/week5/Main.java
+++ src/week5/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Main pStudio = new Main();
-        pStudio.J001();
+        //pStudio.J001();
+        pStudio.J002();
     }
     void J001(){
         int height,weight;
@@ -18,6 +19,17 @@ public class Main {
 
         bmi = weight / (height * 0.01 * height * 0.01);
         System.out.println("bmi : " + bmi);
+
+    }
+    void J002(){
+        double c_degree;
+        double f_degree;
+        System.out.print("섭씨 온도 : ");
+        Scanner in = new Scanner(System.in);
+        c_degree = in.nextDouble();
+        f_degree = c_degree * 1.8 + 32;
+
+        System.out.println("f_degree : " + f_degree);
 
     }
 }
