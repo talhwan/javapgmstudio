2e2534b09e178615a85edb6322dd95a9e5a31361
01 <talhwan@hotmail.com>
2024-04-02 10:00:30 +0900

    add week5

--- src/P1.java
+++ src/P1.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class Week5 {
+public class P1 {
     public static void main(String[] args) {
 
         /*
---
+++ src/week5/Main.java
@@ -0,0 +1,13 @@
+package week5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main a = new Main();
+        a.J001();
+    }
+    void J001(){
+        System.out.println("test");
+    }
+}
